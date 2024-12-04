37806abc6dc5684065a724258ffcce3af22de970
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-12-04 10:35:08 +0530

    Add files via upload

---
+++ 5_javascript/script1.js
@@ -0,0 +1,10 @@
+import { obj as obj1, add } from "./script2.js";
+// import app from './mod2.js';               //this is for default import export syntax
+//import {add} from './mod2.js';              //here this used in naming import export syntax
+// imort * as something from './mod2.js';    //this used for universal import file (all file import)
+
+//console.log(something);
+//console.log(something.obj);
+//console.log(something.add(2,3));
+console.log(obj1);
+console.log(app);
