58ed72c920dd6786a918b5a6853f4bc5b0da87fc
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-12-04 10:34:16 +0530

    Add files via upload

---
+++ 5_javascript/mod1.js
@@ -0,0 +1,12 @@
+//import variable and function from mod2 file
+
+import { obj as obj1, add } from "./mod2.js";
+//import { add } from ./mod2.js';
+
+// import * as something from './mod2.js';
+
+// console.log (something);
+// console.log(something. obj);
+// console.log(something.add(2, 3));
+console.log(obj1);
+console.log(add);
