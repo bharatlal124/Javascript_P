11b3acf2641d7c2aaabe721ebb1efe740d3eed94
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-12-04 10:34:36 +0530

    Add files via upload

---
+++ 5_javascript/mod2.js
@@ -0,0 +1,11 @@
+let obj = { pname: "Mobile", price: 10 };
+
+let a = 20;
+let b = 30;
+
+function add(i, j) {
+  return i + j;
+}
+
+// export function and object to other file 
+export { obj, add };
