ab8c1c903b847c0e5df86b01e5a08c884f6ad526
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-12-17 10:29:44 +0530

    Add files via upload

---
+++ 13_javascript/Reversing a String/script.js
@@ -0,0 +1,9 @@
+//complete the reverseString Function.
+//Do not alter the starter Code.
+
+function reverseString(str){
+//Implement Your function here
+    return str.split("").reverse().join("");
+}
+console.log(reverseString("Hello"));
+//Output : "olleH"
