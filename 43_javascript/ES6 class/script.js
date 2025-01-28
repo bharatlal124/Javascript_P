7a24b3965034998df4d1a55f134972200e99d7ba
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-01-28 10:30:46 +0530

    Add files via upload

---
+++ 43_javascript/ES6 class/script.js
@@ -0,0 +1,43 @@
+// Do not alter the starter code
+function main() {
+    // Implement your class here
+    class Person {
+        constructor(name, age, gender) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        speak() {
+            console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+        }
+    }
+
+    class Student {
+        constructor(person, major, gpa) {
+            this.person = person;
+            this.major = major;
+            this.gpa = gpa;
+        }
+
+        study() {
+            console.log(`I am studying ${this.major} and my GPA is ${this.gpa}.`);
+        }
+
+        speak() {
+            console.log(`Hello, my name is ${this.person.name} and I am ${this.person.age} years old. I am also a student studying ${this.major}.`);
+        }
+    }
+
+    return { Person, Student };
+}
+
+// Example usage:
+const { Person, Student } = main();
+
+const person1 = new Person("John", 20, "M");
+person1.speak();
+
+const stud1 = new Student(person1, "CS", 9);
+stud1.speak();
+stud1.study();
